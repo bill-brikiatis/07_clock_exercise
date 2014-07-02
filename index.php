5001e1f5560e7bbcedbe2c045d60daa217743dbe
bill-brikiatis <brikiatis@g.harvard.edu>
2014-07-02 14:48:25 -0400

    Built Application

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
-	date_default_timezone_set('America/New_York');
+	$time_zone = 'America/New_York';
+	date_default_timezone_set($time_zone);
 	$current_time = date("g:ia");
+	$military_time = date("G");
+	$military_time = 23; /* test */
+	if ($military_time >= 5 && $military_time < 11) {
+		$class="morning";
+	}
+	elseif ($military_time >= 11 && $military_time < 16) {
+		$class="afternoon";
+	}
+	elseif ($military_time >= 16 && $military_time < 20) {
+		$class="evening";
+	}
+	else {
+		$class="night";
+	}
 ?>
 
 <!doctype html>
@@ -9,17 +24,24 @@
 <head>
   <meta charset="utf-8">
 
-  <title>The Clock Exercise</title>
+  <title>What Time is It</title>
   <meta name="description" content="Bill's Clock Exercise">
   <meta name="author" content="Bill Brikiatis">
 
-  <link rel="stylesheet" href="css/styles.css?v=1.0">
+  <link rel="stylesheet" href="css/style.css">
 
   <!--[if lt IE 9]>
   <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
 </head>
 
-<body>
-  <?php echo $current_time; ?></body>
+<body class="<?php echo $class ?>">
+		<div class="<?php echo $class; ?>">
+		  	<?php 
+		  	echo "<h1>It is $current_time</h1>
+		  	<small>Time zone: $time_zone;</small><br />
+		  	<img src='images/$class.png' alt='Scenery matching the time of day'";
+		  	?>
+		</div> <!-- END .$class -->
+</body>
 </html>
